3ece20ce7ec0894f09eff15fc978c204cabadfe9
thompa21 <tholind@kth.se>
2023-08-10 11:34:43 +0200

    sql parameter publicationdate

--- Models.js
+++ Models.js
@@ -3,53 +3,55 @@ const database = require('./db');
 //Hämta alla böcker från och med aktiveringsdatum
 const readNewbooks = (req) => {
 
-  let activationdate = req.query.activationdate || '2000-01-01';
-  let sql = `SELECT id, mmsid, recordid, isbn, isbnprimo, thumbnail, coverurl, 
-                  title, DATE_FORMAT(activationdate, "%Y-%m-%d") as activationdate, 
-                  publicationdate, dewey, subject, category, subcategory, booktype 
-                  FROM newbooks
-                  WHERE activationdate >= ?
-                  ORDER BY activationdate DESC`;
-
-    /*
-  if(req.query.activationdate) {
-      sql += ` AND activationdate >= '${req.query.activationdate}'`
-  }
-
-  if(req.query.publicationdate) {
-      sql += ` AND  cast(publicationdate AS SIGNED) >= ${req.query.publicationdate}`
-  }
-
-  if(req.query.booktype) {
-      sql += ` AND booktype = '${req.query.booktype}'`
-  }
-
-  if(req.query.dewey) {
-      if (req.query.dewey.length == 1) {
-          sql += ` AND (dewey LIKE '${req.query.dewey}__.%' OR dewey LIKE '${req.query.dewey}__/%')`
-      }
-      if (req.query.dewey.length == 2) {
-          sql += ` AND (dewey LIKE '${req.query.dewey}_.%' OR dewey LIKE '${req.query.dewey}_/%')`
-      }
-      if (req.query.dewey.length == 3) {
-          sql += ` AND (dewey LIKE '${req.query.dewey}.%' OR dewey LIKE '${req.query.dewey}/%')`
-      }
-      
-  }
-  */
-
-  //sql += ` ORDER BY activationdate DESC`;
-
-  return new Promise(function (resolve, reject) {    
-      database.db.query(database.mysql.format(sql,[activationdate]),(err, result) => {
-          if(err) {
-            console.error('Error executing query:', err);
-            reject(err.message)
-          }
-          const successMessage = "Success"
-          resolve(result);
-      });
-  })
+    let activationdate = req.query.activationdate || '2020-01-01';
+    let publicationdate = req.query.publicationdate || '2020';
+    let sql = `SELECT id, mmsid, recordid, isbn, isbnprimo, thumbnail, coverurl, 
+                    title, DATE_FORMAT(activationdate, "%Y-%m-%d") as activationdate, 
+                    publicationdate, dewey, subject, category, subcategory, booktype 
+                    FROM newbooks
+                    WHERE activationdate >= ?
+                    AND  cast(publicationdate AS SIGNED) >= ?
+                    ORDER BY activationdate DESC`;
+  
+      /*
+    if(req.query.activationdate) {
+        sql += ` AND activationdate >= '${req.query.activationdate}'`
+    }
+  
+    if(req.query.publicationdate) {
+        sql += ` AND  cast(publicationdate AS SIGNED) >= ${req.query.publicationdate}`
+    }
+  
+    if(req.query.booktype) {
+        sql += ` AND booktype = '${req.query.booktype}'`
+    }
+  
+    if(req.query.dewey) {
+        if (req.query.dewey.length == 1) {
+            sql += ` AND (dewey LIKE '${req.query.dewey}__.%' OR dewey LIKE '${req.query.dewey}__/%')`
+        }
+        if (req.query.dewey.length == 2) {
+            sql += ` AND (dewey LIKE '${req.query.dewey}_.%' OR dewey LIKE '${req.query.dewey}_/%')`
+        }
+        if (req.query.dewey.length == 3) {
+            sql += ` AND (dewey LIKE '${req.query.dewey}.%' OR dewey LIKE '${req.query.dewey}/%')`
+        }
+        
+    }
+    */
+  
+    //sql += ` ORDER BY activationdate DESC`;
+  
+    return new Promise(function (resolve, reject) {    
+        database.db.query(database.mysql.format(sql,[activationdate, publicationdate]),(err, result) => {
+            if(err) {
+              console.error('Error executing query:', err);
+              reject(err.message)
+            }
+            const successMessage = "Success"
+            resolve(result);
+        });
+    })
 };
 
 //Hämta användarens högsta löpnummer
