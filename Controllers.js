a456c442d9e10d48946451b720c762ec335d83a5
Thomas Lind <tholind@kth.se>
2024-11-19 14:49:11 +0100

    librisls 035

--- Controllers.js
+++ Controllers.js
@@ -32,13 +32,16 @@ async function getlibrisLS(req, res) {
         let responsexml = `<?xml version="1.0" encoding="UTF-8" ?>
 					<Item_Information xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://appl.libris.kb.se/LIBRISItem.xsd">`
 	
-        if ((req.query.ISBN || req.query.ONR || req.query.ISSN) && req.query.library) {
-            if (req.query.ISBN != '') {
+        // I Alma ligger ofta libris ONR i ISBN-fältet. Men det ligger alltid i 035. i 035 ligger också bib_id. Så sök i other_system_number
+        if ((req.query.ISBN || req.query.ONR || req.query.ISSN || req.query.BIB_ID) && req.query.library) {
+            if (req.query.ISBN && req.query.ISBN.trim() !== '') {
                 sru = `https://eu01.alma.exlibrisgroup.com/view/sru/46KTH_INST?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=alma.isbn="${req.query.ISBN}"&maximumRecords=10`
-            } else if (req.query.ONR != '') {
-                sru = `https://eu01.alma.exlibrisgroup.com/view/sru/46KTH_INST?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=alma.isbn="${req.query.ONR}"&maximumRecords=10`
-            } else if (req.query.ISSN != '') {
+            } else if (req.query.ISSN && req.query.ISSN.trim() !== '') {
                 sru = `https://eu01.alma.exlibrisgroup.com/view/sru/46KTH_INST?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=alma.issn="${req.query.ISSN}"&maximumRecords=10`
+            } else if (req.query.ONR && req.query.ONR.trim() !== '') {
+                sru = `https://eu01.alma.exlibrisgroup.com/view/sru/46KTH_INST?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=alma.other_system_number="${req.query.ONR}"&maximumRecords=10`
+            } else if (req.query.BIB_ID && req.query.BIB_ID.trim() !== '') {
+                sru = `https://eu01.alma.exlibrisgroup.com/view/sru/46KTH_INST?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=alma.other_system_number="${req.query.BIB_ID}"&maximumRecords=10`
             }
             if(sru != '') {
                 const response = await axios.get(sru)
